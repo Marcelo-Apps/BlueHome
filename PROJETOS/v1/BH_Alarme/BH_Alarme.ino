960580362fbaf6796c0c43d2a0f2d45d6e3facd9
Marcelo Costa <mgdc.apps@gmail.com>
2019-10-26 12:26:27 -0300

    BH-Alarme com código do Felipe. Opção de desabilitar o Blynk Criada. Por padrão Blynk está desabilitado. Atualização da lista das portas dos microcontroladores.

--- PROJETOS/v1/BH_Alarme/BH_Alarme.ino
+++ PROJETOS/v1/BH_Alarme/BH_Alarme.ino
@@ -1,249 +1,263 @@
 /*
- * BLUE HOME - V1.0
- * 
- * PROCESSADOR DE ALARME
- * ESP-32 COM 30 PINOS
- * 
- * Autores: Marcelo Costa / Felipe Maia / Faberson Perfeito / Newton Dore
- * 
- * OBS: Ainterface é feita pelo Blynk. Para maiores informações consultar o PDF que acompanha o projeto
- */
+   BLUE HOME - V1.0
+
+   PROCESSADOR DE ALARME
+   ESP-32 COM 30 PINOS
+
+   Autores: Marcelo Costa / Felipe Maia / Faberson Perfeito / Newton Dore
+
+   OBS: Ainterface é feita pelo Blynk. Para maiores informações consultar o PDF que acompanha o projeto
+*/
+
+
+// Se estiver definido não inicializa/processa o BLYNK
+#define SEMBLYNK
 
 
 // Se é depuração (gera conteudo na serial para análise)
 #define DEBUG
 
 
-#ifdef DEBUG  
-  #define BLYNK_PRINT Serial
+#ifdef DEBUG
+#define BLYNK_PRINT Serial
 #endif
-
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <SPI.h>
-#include <MFRC522.h>
-
+#include <Stepper.h> //INCLUSÃO DE BIBLIOTECA MOTORES
 
 // Parâmetros da Wi-Fi
 char ssid[] = "WIFI-BH";
 char pass[] = "BlueHome.IoT.2019";
 
-
 // Chave de autenticação do Projeto no Blynk
 //char auth[] = "Xk9Gxg4mK9pfkGEODlVaaqeyZdcNZLXZ";      // Automação
 char auth[] = "UhhrKzVPwBwg-ByLNCthYxpNMTZgK41l";      // Alarme
 
-// Pinos usados para outros fins
-// D16 -- U2_RX
-// D17 -- U2_TX
-// D18 -- SPI_SCK
-// D19 -- SPI_MSIO
-// D23 -- SPI_MIOSI
-// D05 -- Aviso Alarme--Automacao
-// D04 - RESERVADO ENQUANTO FAZ COMUNICACAO SERIAL COM PC
-//
-
-
-
 #define PIN_SENSORPIR              36
+#define PIN_ALARME                  4
+
 #define PIN_SENSORJANELA           35
 #define PIN_SENSORPORTA            34
-#define PIN_ALARME                  4
-#define PIN_MOTORJANELA_I1         23
-/// MAIS DEFINES
-/// MAIS DEFINES
-/// MAIS DEFINES
-/// MAIS DEFINES
 
+#define PIN_CMDABRIRJANELA         26
+#define PIN_CMDFECHARJANELA        25
+#define PIN_CMDABRIRPORTA          33
+#define PIN_CMDFECHARPORTA         32
 
+#define PIN_MOTORJANELA_I1         27
+#define PIN_MOTORJANELA_I2         14
+#define PIN_MOTORJANELA_I3         12
+#define PIN_MOTORJANELA_I4         13
 
+#define PIN_MOTORPORTA_I1         2
+#define PIN_MOTORPORTA_I2         18
+#define PIN_MOTORPORTA_I3         19
+#define PIN_MOTORPORTA_I4         23
 
 #define BLYNK_LCD                  V0
-
-
 #define BLYNK_JANELA              V11
-
-
-
-#define MOTOR_PASSOS-PORTA         20
-#define MOTOR_PASSOS-JANELA        25
-
 
 // *VARIÁVEIS GLOBAIS*
 char *_lcdLin1;
 char *_lcdLin2;
 
-bool _atualSensorLuz, _atualLuzExt, _atualLuzAutomatica;
-bool _novoSensorLuz, _novoLuzExt, _novoLuzAutomatica;
+bool _portaEmMovimento;
+bool _janelaEmMovimento;
 
+int _passoJanelaAtual; //passo atual da porta
+int _passoPortaAtual; //passo atual da porta
 
+const int _passosPorVoltaJanela = 30; //NÚMERO DE PASSOS POR VOLTA DO MOTOR DA JANELA
+const int _passosPorVoltaPorta = 30; //NÚMERO DE PASSOS POR VOLTA DO MOTOR DA PORTA
+
+Stepper motorJanela(_passosPorVoltaJanela, PIN_MOTORJANELA_I1, PIN_MOTORJANELA_I3, PIN_MOTORJANELA_I2, PIN_MOTORJANELA_I4); //INICIALIZA O MOTOR JANELA
+Stepper motorPorta(_passosPorVoltaPorta, PIN_MOTORPORTA_I1, PIN_MOTORPORTA_I3, PIN_MOTORPORTA_I2, PIN_MOTORPORTA_I4); //INICIALIZA O MOTOR PORTA
 
 void setup() {
-
-  // Se DEBUG, inicializa a serial
 #ifdef DEBUG
   Serial.begin(115200);
 #endif
 
+  motorJanela.setSpeed(300); //VELOCIDADE DO MOTOR JANELA
+  motorPorta.setSpeed(300); //VELOCIDADE DO MOTOR PORTA
+
   // INICIALIZAÇÃO DO AMBIENTE
   pinMode(PIN_SENSORPIR, INPUT);
-// CONFIGURAR MAIS PINOS
-// CONFIGURAR MAIS PINOS
-// CONFIGURAR MAIS PINOS
-// CONFIGURAR MAIS PINOS
-// CONFIGURAR MAIS PINOS
+  pinMode(PIN_ALARME, OUTPUT);
 
-  
+  pinMode(PIN_SENSORJANELA, INPUT);
+  pinMode(PIN_SENSORPORTA, INPUT);
+
+  pinMode(PIN_CMDABRIRJANELA, INPUT);
+  pinMode(PIN_CMDFECHARJANELA, INPUT);
+  pinMode(PIN_CMDABRIRPORTA, INPUT);
+  pinMode(PIN_CMDFECHARPORTA, INPUT);
+
   // Inicializa os parâmetros e ajustes iniciais
-  inicializaAjustes();
+  inicializaContexto();
 
+#ifndef SEMBLYNK
   // Inicializa o Blynk
   Blynk.begin(auth, ssid, pass);
-
+#endif
 }
 
 
 
 void loop() {
-  Blynk.run();
-
-  verificaParamLuzAutomatica();
-  verificaSensorLuz();
-  atuaSensorLuz();
-
-  delay(100);
-}
-
-
-
-void inicializaAjustes (void) {
-  _atualSensorLuz=false;
-  _novoSensorLuz=false;
-  _atualLuzExt=false;
-  _novoLuzExt=false;
-  _atualLuzAutomatica=true;
-  _novoLuzAutomatica=true;
-  
-
-  _lcdLin1="";
-  _lcdLin2="";
-
-  digitalWrite(PIN_LUZEXTERNA,HIGH);
-
-}
-
-
-
-void verificaParamLuzAutomatica (void) {
-  if (_atualLuzAutomatica!=_novoLuzAutomatica) {
-#ifdef DEBUG
-     Serial.print("-- Mudou o Estado do Parâmetro Luz Automática");
-     Serial.print(" -- NOVO estado: ");
-     Serial.println(_novoLuzAutomatica);
+#ifndef SEMBLYNK
+    Blynk.run();
 #endif
+
+  if (!_portaEmMovimento) {
+    cmdAbrirPorta();
+    cmdFecharPorta();
+  } else {
+    atuaNaPorta();
+    verificaSensorPorta();
+  }
+
+  if (!_janelaEmMovimento) {
+    cmdAbrirJanela();
+    cmdFecharJanela();
+  } else {
+    atuaNaJanela();
+    verificaSensorJanela();
   }
 }
 
 
 
+// Inicializa o Ambiente (variáveis)
+void inicializaContexto (void) {
+  _portaEmMovimento = false;
+  _janelaEmMovimento = false;
 
-// Lê o sensor de Luz SE JÁ não estiver modificado
-void verificaSensorLuz (void) {
-  if (_atualSensorLuz==_novoSensorLuz) {
-    _novoSensorLuz=(digitalRead(PIN_SENSORLUZ)==HIGH);
+  _lcdLin1 = "";
+  _lcdLin2 = "";
+}
+
+
+
+void cmdAbrirPorta(void) {
+  if (digitalRead(PIN_CMDABRIRPORTA)) {
+    _passoPortaAtual = 30;
+    _portaEmMovimento = true;
   }
 }
 
 
-// Atua no sensor de Luz SE Precisar
-void atuaSensorLuz (void) {
-  if ((_atualSensorLuz!=_novoSensorLuz)  || (_atualLuzAutomatica!=_novoLuzAutomatica)) {
 
-    // Faz a atribuição de Atual=Novo
-    _atualSensorLuz=_novoSensorLuz;
-    _atualLuzAutomatica=_novoLuzAutomatica;
+void cmdFecharPorta(void) {
+  if (digitalRead(PIN_CMDFECHARPORTA)) {
+    _passoPortaAtual = -1;
+    _portaEmMovimento = true;
+  }
+}
 
-#ifdef DEBUG
-      Serial.println("-- Mudou o Estado do Sensor de Luz");
-#endif
 
-  if (_atualLuzAutomatica) {
-      // Executa a ação
-      if (_atualSensorLuz) {
-        digitalWrite(PIN_LUZEXTERNA,LOW);
-#ifdef DEBUG
-        Serial.println(" -- Ligou a Luz Externa");
-#endif
-      } else {
-        digitalWrite(PIN_LUZEXTERNA,HIGH);
-#ifdef DEBUG
-        Serial.println(" -- Desligou a Luz Externa");
-#endif
-      }
-    } else {
-#ifdef DEBUG
-      Serial.println(" -- *NÃO FEZ NADA -- LUZ AUTOMÁTICA ESTÁ DESLIGADA*");
-#endif
+
+void cmdAbrirJanela(void) {
+  if (digitalRead(PIN_CMDABRIRJANELA)) {
+    _passoJanelaAtual = 30;
+    _janelaEmMovimento = true;
+  }
+}
+
+
+
+void cmdFecharJanela(void) {
+  if (digitalRead(PIN_CMDFECHARJANELA)) {
+    _passoJanelaAtual = -1;
+    _janelaEmMovimento = true;
+  }
+}
+
+
+
+// Lê o sensor da janela se ja fechou ou abriu totalmente
+void verificaSensorJanela (void) {
+  if (digitalRead(PIN_SENSORJANELA)) { //se detectou que a janela ja fechou ou ja abriu totalmente
+    if (_passoJanelaAtual > 0) { //estava em processo de abrir e ja detectou que abriu totalmente
+      _passoJanelaAtual = 0;
+
+    } else if (_passoJanelaAtual < 0 && _passoJanelaAtual > -30) { // estava em processo de fechar e ja detectou que fechou totalmente
+      _passoJanelaAtual = -30;
     }
   }
+}
 
+
+
+// Lê o sensor da porta se ja fechou ou abriu totalmente
+void verificaSensorPorta (void) {
+  if (digitalRead(PIN_SENSORPORTA)) { //se detectou que a porta ja fechou ou ja abriu totalmente
+    if (_passoPortaAtual > 0) { //estava em processo de abrir e ja detectou que abriu totalmente
+      _passoPortaAtual = 0;
+
+    } else if (_passoPortaAtual < 0 && _passoPortaAtual > -30) { // estava em processo de fechar e ja detectou que fechou totalmente
+      _passoPortaAtual = -30;
+    }
+  }
+}
+
+void atuaNaPorta (void) {
+
+  if (_passoPortaAtual > 0) { //porta abrindo
+    motorPorta.step(_passosPorVoltaPorta);
+    _passoPortaAtual--;
+
+  } else if (_passoPortaAtual == 0) {
+    _portaEmMovimento = false; //porta permanece aberta
+
+  } else if (_passoPortaAtual <= 0 && _passoPortaAtual > -30) { //porta fechando
+    motorPorta.step(-_passosPorVoltaPorta);
+    _passoPortaAtual--;
+  } else {
+    _portaEmMovimento = false; // porta permanece fechada
+  }
+}
+
+
+
+void atuaNaJanela(void) {
+
+  if (_passoJanelaAtual > 0) { //janela abrindo
+    motorJanela.step(_passosPorVoltaJanela);
+    _passoJanelaAtual--;
+
+  } else if (_passoJanelaAtual == 0) {
+    _janelaEmMovimento = false; //janela permanece aberta
+
+  } else if (_passoJanelaAtual <= 0 && _passoJanelaAtual > -30) { //janela fechando
+    motorJanela.step(-_passosPorVoltaJanela);
+    _passoJanelaAtual--;
+  } else {
+    _janelaEmMovimento = false; // janela permanece fechada
+  }
 }
 
 
 
 void printLCD (char texto[]) {
+#ifndef SEMBLYNK
   WidgetLCD lcd(V0);
-
   _lcdLin1=_lcdLin2;
   _lcdLin2=texto;
   lcd.clear();
   lcd.print(0,0,_lcdLin1);
   lcd.print(0,1,_lcdLin2);
-}
-
-
-
-
-///
-
-
-// Redefine o ambiente quando conectado.
-BLYNK_CONNECTED () {
-  // Manda o texto
-  printLCD("BLUEHOME - UTFPR");
-  printLCD("ATIVO");
-}
-
-
-
-// MUDANÇA DE COR DA LUZ INTERNA
-BLYNK_WRITE(BLYNK_LUZINTERNA) {
-  int red, green, blue;
-
-  // Lê parâmetros dos Blynk
-  red=param[0].asInt();
-  green=param[1].asInt();
-  blue=param[2].asInt();
-
-#ifdef DEBUG
-  Serial.print("--LUZ INTERNA - Red: ");
-  Serial.println(red);
-  Serial.print(",  Green: ");
-  Serial.println(green);
-  Serial.print(",  Blue: ");
-  Serial.println(blue);
 #endif
-  
-  ledcWrite(PWM_LEDRED,red);
-  ledcWrite(PWM_LEDGREEN,green);
-  ledcWrite(PWM_LEDBLUE,blue);
 }
 
 
 
-// Apenas lê o conteúdo da Luz Automática (a atualização é no novo ciclo)
-BLYNK_WRITE(BLYNK_LUZAUTOMATICA) {
-  _novoLuzAutomatica=(param.asInt()!=0);
+//Redefine o ambiente quando conectado.
+BLYNK_CONNECTED () {
+  // Manda o texto para o LCD
+  printLCD("BH-ALARME OK");
+//  printLCD("");
 }
